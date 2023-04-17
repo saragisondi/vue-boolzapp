fb423f47461d10a01d567f737d9d3e620106dc0b
Sara Gisondi <sara.gisondi@outlook.it>
2023-04-17 15:50:30 +0200

    add contacts

--- Milestone 1/js/script.js
+++ Milestone 1/js/script.js
@@ -3,7 +3,48 @@ const {createApp} = Vue;
 createApp({
   data(){
     return{
-      
+      contacts:[
+        {
+          name:'Michele',
+          avatar:'img/avatar_1.jpg',
+          visible:true,
+        },
+          {
+          name:'Fabio',
+          avatar:'img/avatar_2.jpg',
+          visible:true,
+        },
+          {
+          name:'Samuele',
+          avatar:'img/avatar_3.jpg',
+          visible:true,
+        },
+          {
+          name:'Alessandro B.',
+          avatar:'img/avatar_4.jpg',
+          visible:true,
+        },
+          {
+          name:'Alessandro L.',
+          avatar:'img/avatar_5.jpg',
+          visible:true,
+        },
+          {
+          name:'Claudia',
+          avatar:'img/avatar_6.jpg',
+          visible:true,
+        },
+          {
+          name:'Federico',
+          avatar:'img/avatar_7.jpg',
+          visible:true,
+        },
+          {
+          name:'Michele',
+          avatar:'img/avatar_8.jpg',
+          visible:true,
+        },
+      ]
     }
   }
 }).mount('#app')
